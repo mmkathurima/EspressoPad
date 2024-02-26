3f25b94518e0893109665c3d9425b1ce6014937f
titans068 <mwimakat@yahoo.com>
2024-02-27 00:49:45 +0300

    Better looking echo output.

--- src/main/java/com/github/espressopad/ui/About.java
+++ src/main/java/com/github/espressopad/ui/About.java
@@ -84,7 +84,7 @@ public class About extends Application {
         productName.setFont(Font.font(Font.getDefault().getFamily(), FontWeight.EXTRA_BOLD, 36d));
         productName.setAlignment(Pos.CENTER);
 
-        Label details = new Label(String.format("v0.23.2\n©%d\nRuntime: %s %s %s\nVM: %s", Year.now().getValue(),
+        Label details = new Label(String.format("v0.23.3\n©%d\nRuntime: %s %s %s\nVM: %s", Year.now().getValue(),
                 System.getProperty("java.vm.vendor"), System.getProperty("java.vm.version"),
                 System.getProperty("os.arch"), System.getProperty("java.vm.name")));
         details.setAlignment(Pos.CENTER);
@@ -231,8 +231,8 @@ public class About extends Application {
                 tableView.getItems().setAll(properties.stream()
                         .filter(x -> x.entrySet()
                                 .stream()
-                                .anyMatch(y -> y.getKey().contains(filterProperty.getText())
-                                        || y.getValue().contains(filterProperty.getText())))
+                                .anyMatch(y -> y.getKey().toLowerCase().contains(filterProperty.getText().toLowerCase())
+                                        || y.getValue().toLowerCase().contains(filterProperty.getText().toLowerCase())))
                         .collect(Collectors.toList()));
                 tableView.getSortOrder().setAll(keyColumn);
             }
